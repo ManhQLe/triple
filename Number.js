8cb2de9781ba2e724ff6ffb227de1dd507ba8f6e
Manh Le <quang.manh@gmail.com>
2018-04-16 00:51:22 -0500

    Added GCF

--- Number.js
+++ Number.js
@@ -1,4 +1,19 @@
-const epsilon = e-8
-export function isSame(a, b) {
+const epsilon = 1e-8
+module.exports={
+    isSame(a, b) {
     return Math.abs(a-b) <=epsilon
+    },
+    GCF(numbers){
+        let gcf = numbers[0];
+        let i = 1;
+        while(i<numbers.length){
+            let b = numbers[i++],a = gcf,x
+            do{
+                x = a - b;
+                x>0?a=x:(x=-x,b=x);
+                x?gcf=x:1
+            }while(x)        
+        }
+        return gcf
+    }
 }
