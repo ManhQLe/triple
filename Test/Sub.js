f0358a5d7cdc55cf187fa6a760dc37e43d227ec7
Manh Le <quang.manh@gmail.com>
2018-04-22 19:59:52 -0500

    Added sub

---
+++ Test/Sub.js
@@ -0,0 +1,12 @@
+const assert = require("assert");
+const Triple = require("../src/Triple")
+
+const t1 = new Triple(4,3,5);
+const t2 = new Triple(3,4,5)
+
+const t3 = t2.sub(t1)
+const expect = new Triple(24,7,25)
+
+console.log(t3)
+
+assert.equal(t3.equals(expect),true);
