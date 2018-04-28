46f02024c6b94fce9bd457fb3f3dc05c0463fdc3
Manh Le <quang.manh@gmail.com>
2018-04-27 21:05:16 -0500

    construct Triple from sine and cosine

--- src/Triple.js
+++ src/Triple.js
@@ -1,10 +1,9 @@
 const Number = require('./Number')
 
-class Triple
-{
-    constructor(b=0,h=1,d=1){
+class Triple {
+    constructor(b = 0, h = 1, d = 1) {
 
-        if(!Number.isSame(b*b + h*h,d*d))
+        if (!Number.isSame(b * b + h * h, d * d))
             throw "Invalid triple"
         this.b = b;
         this.h = h;
@@ -12,97 +11,107 @@ class Triple
 
     }
 
-    unit(){
-        return new Triple(this.cos(),this.sin(),1);
+    unit() {
+        return new Triple(this.cos(), this.sin(), 1);
     }
 
-    tan(){
-        return this.h/this.b
+    tan() {
+        return this.h / this.b
     }
 
-    sin(){
-        return this.h/this.d
+    sin() {
+        return this.h / this.d
     }
 
-    cos(){
-        return this.b/this.d
+    cos() {
+        return this.b / this.d
     }
 
-    cosec(){
-        return this.d/this.h
+    cosec() {
+        return this.d / this.h
     }
 
-    sec(){
-        return this.d/this.b
+    sec() {
+        return this.d / this.b
     }
 
-    cotan(){
-        return this.b/this.h
+    cotan() {
+        return this.b / this.h
     }
 
-    angle(){
-        return Math.atan2(this.h,this.b);
+    angle() {
+        return Math.atan2(this.h, this.b);
     }
 
-    add(x){
+    add(x) {
         return new Triple(
-            this.b*x.b - this.h*x.h,
-            this.h*x.b + this.b* x.h,
+            this.b * x.b - this.h * x.h,
+            this.h * x.b + this.b * x.h,
             this.d * x.d
         )
     }
 
-    sub(x){
+    sub(x) {
         return new Triple(
-            this.b*x.b + this.h*x.h,
-            this.h*x.b - this.b* x.h,
+            this.b * x.b + this.h * x.h,
+            this.h * x.b - this.b * x.h,
             this.d * x.d
         )
     }
 
-    equals(x){
-        return Number.isSame(x.b,this.b)
-        && Number.isSame(x.h,this.h)
-        && Number.isSame(x.d,this.d)
+    equals(x) {
+        return Number.isSame(x.b, this.b) &&
+            Number.isSame(x.h, this.h) &&
+            Number.isSame(x.d, this.d)
     }
 
-    toString(){
+    toString() {
         return `${this.b} ${this.h} ${this.d}`
     }
 
-    rotateRad(rad=0){
+    rotateRad(rad = 0) {
         return this.add(Triple.fromRad(rad));
     }
 
-    rotate(ang=0){
+    rotate(ang = 0) {
         return this.rotateRad(Number.toRad(ang))
     }
 
 
-    radAngle(){
-        return Math.atan2(this.h,this.b);
+    radAngle() {
+        return Math.atan2(this.h, this.b);
     }
 
-    angle(){
-        return Number.toAngle(Math.atan2(this.h,this.b))
+    angle() {
+        return Number.toAngle(Math.atan2(this.h, this.b))
     }
 
 
 
-    static fromRad(rad){
-        return new  Triple(Math.cos(rad),Math.sin(rad))
+    static fromRad(rad) {
+        return new Triple(Math.cos(rad), Math.sin(rad))
     }
 
-    static fromAngle(ang){
+    static fromAngle(ang) {
         return Triple.fromRad(Number.rotateRad(ang))
-    }    
+    }
+
+    static fromSineOf(rad) {
+        let sin = Math.sin(rad)
+        return new Triple(Math.sqrt(1 - sin * sin), sin, 1)
+    }
+    static fromCosineOf(rad) {
+        let cos = Math.cos(rad)
+        return new Triple(cos, Math.sqrt(1 - cos * cos), 1)
+    }
 }
 
 
 
-Triple.COMMON = new Proxy(Triple,{
-    get:function(obj,key){
-        switch(key){
+
+Triple.COMMON = new Proxy(Triple, {
+    get: function (obj, key) {
+        switch (key) {
             case "T90":
                 return obj.fromAngle(90);
             case "TN90":
@@ -114,9 +123,9 @@ Triple.COMMON = new Proxy(Triple,{
             case "T60":
                 return obj.fromAngle(60);
             case "T30":
-                return obj.fromAngle(30);                
+                return obj.fromAngle(30);
             case "T45":
-                return obj.fromAngle(45);            
+                return obj.fromAngle(45);
         }
     }
 })
